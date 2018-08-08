e10530a19cbac3b371f7e056b048b34e97782436
awaller2018 <>
2018-08-08 15:52:26 -0700

    Reinstalled vrtk, added prefabs

---
+++ assets/Scripts/VolControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class VolControl : MonoBehaviour {
+
+    private VRTK_Control_UnityEvents controlEvents;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        controlEvents = GetComponent<VRTK_Control_UnityEvents>();
+        if (controlEvents == null)
+        {
+            controlEvents = gameObject.AddComponent<VRTK_Control_UnityEvents>();
+        }
+
+        controlEvents.OnValueChanged.AddListener(HandleChange);
+    }
+
+    private void HandleChange(object sender, Control3DEventArgs e)
+    {
+        Audio_Manager.instance.totVol = e.normalizedValue();
+    }
+}
